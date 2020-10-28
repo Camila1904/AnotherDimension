3fde709394c175e77e7aa07e39dd57a85b4d2a03
Camila Contreras <ccontreras20@stuy.edu>
2020-10-27 19:54:52 -0700

    Sum works

---
+++ Tester.java
@@ -0,0 +1,11 @@
+public class Tester {
+    public static void main (String[] args) {
+        int[] SumTester1 = {1,2,3,4};
+        int[] SumTester2 = {};
+        int[] SumTester3 = {5,6,7};
+        System.out.println (ArrayOpps.sum(SumTester1));
+        System.out.println (ArrayOpps.sum(SumTester2));
+        System.out.println (ArrayOpps.sum(SumTester3));
+        System.out.println();
+    }
+}
